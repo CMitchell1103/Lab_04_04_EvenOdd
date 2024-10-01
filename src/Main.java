9644afb4c73b4e47d96a8388d435c605a3fd618d
CMitchell1103 <mitch3cd@mail.uc.edu>
2024-09-30 23:09:45 -0400

    New final commit and push

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         int numToExamine = 2;
+        int intModulo = 0;
 
-        System.out.println("The number is odd" + numToExamine);
+        if (numToExamine % 2 == 0) {
+        System.out.println("The number is even " + numToExamine); }
+        Object elseIf; {
+        System.out.println("The number is odd" + numToExamine); }
 
     }
 }
